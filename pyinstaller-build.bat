322ffb8bb0a16b9977943fd7970dda69e7e1e4e2
✨小透明・宸✨ <1272742391@qq.com>
2021-01-17 14:08:04 +0800

    Update pyinstaller-build.bat

--- pyinstaller-build.bat
+++ pyinstaller-build.bat
@@ -1,5 +1,5 @@
 @echo off
 python generate-version.py > _version.py
-pyinstaller --clean --console --onefile --icon=NONE --upx-exclude=vcruntime140.dll --hidden-import=_version stuhealth-cli.py
+pyinstaller --clean --console --onefile --icon=NONE --upx-exclude=vcruntime140.dll stuhealth-cli.py
 del _version.py
 pause
